7fac1d78f59df1d134e3278f59e95526a37629b5
j178 <10510431+j178@users.noreply.github.com>
2022-10-30 18:17:18 +0800

    fix go generate

--- swagger_ui.go
+++ swagger_ui.go
@@ -2,5 +2,5 @@ package swagger_ui
 
 import "github.com/j178/swagger-ui/internal/statik"
 
-//go:generate go run github.com/FZambia/statik/fs@master -src=./swagger-ui -dest=./internal -f
+//go:generate go run github.com/FZambia/statik@master -src=./swagger-ui -dest=./internal -f
 var FS = statik.FS
